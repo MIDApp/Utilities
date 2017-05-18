4b7518964fb2eca19a8a1c812a70d03f28550eb1
Alessio Anesa <alessio.anesa@midainformatica.it>
2017-05-18 17:35:15 +0200

    Fixed Copyright

--- AutosizeTableView.h
+++ AutosizeTableView.h
@@ -3,7 +3,7 @@
 //  DUC
 //
 //  Created by Alessio Anesa on 24/04/15.
-//  Copyright (c) 2015 moma comunicazione. All rights reserved.
+//  Copyright (c) 2015 MIDApp s.r.l.. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
